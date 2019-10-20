d4fc71674a22d62639835f44fd1ab0622ec43ac2
Michael Hiiva <michaelhiiva@gmail.com>
2019-10-20 17:23:11 +0000

    Remove includes from kabocha theme.

--- includes/drupal.wphead.generate.php
+++
@@ -1,9 +0,0 @@
-<?php
-$wphead_news_directory = dirname(__DIR__, 2) . "/news";
-require($wphead_news_directory . '/wp-blog-header.php');
-
-?>
-      <link rel="stylesheet" type="text/css" href="/theme/wordpress/style.css" />
-<?php
-      wp_head();
-?>
